6c40a0ba877172c2c67fe4908a912ab7d1f0b9f5
Curson mañana <Curson maana@PC03a5>
2018-01-29 11:10:45 +0100

    cambio para javamanszzzzz123

--- 65oscara/src/basico/Prueba.java
+++ 65oscara/src/basico/Prueba.java
@@ -5,7 +5,7 @@ public class Prueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hola, estas mas perdido que un cabron");
-System.out.println("adios, encuentrate javaman");
+System.out.println("adios, encuentrate javaman1");
 	}
 
 }
