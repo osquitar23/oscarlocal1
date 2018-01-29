f655d31c8c38ea2ac0a0e6cae512e4bb428e8027
Curson mañana <Curson maana@PC03a5>
2018-01-29 10:20:13 +0100

    213

--- 12_davideclipsegit/src/basico/Saludar.java
+++ 12_davideclipsegit/src/basico/Saludar.java
@@ -5,7 +5,7 @@ public class Saludar {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hola, soy David y uso eclipse para probar");
+		System.out.println("Hola, soy David y te modifico los ficheros");
 		
 	}
 
